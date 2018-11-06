cb2e87a8842ffce59a8f5da3c1f9941af5e97743
github username <github email address>
2018-11-06 11:16:45 -0500

    commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ControlledInput from './components/ControlledInput';
+
+// ReactDOM.render(
+//   <ControlledInput/>,
+//   document.getElementById('root')
+// );
+
 
 ReactDOM.render(
-  <div>Mount Components Here</div>,
+  <ControlledInput />,
   document.getElementById('root')
 );
